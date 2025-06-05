a6fe4efc4c83f763595521154989551d0cd22a1b
psy0524 <parksangy@naver.com>
2025-06-05 16:49:35 +0900

    유니티 에러 해결

--- Assets/02. Scripts/Study/StudyWhileLoop.cs
+++ Assets/02. Scripts/Study/StudyWhileLoop.cs
@@ -1,22 +1,25 @@
 using UnityEngine;
 
-public class WhileLoop : MonoBehaviour
+public class StudyWhileLoop : MonoBehaviour
 {
-    private int count = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private int count = 0;
+
     void Start()
     {
         while (count <= 10)
         {
-            if(count % 3 == 0) // 나머지 연산 3의 배수
+            count++;
+
+            // 3의 배수 조건
+            if (count % 3 == 0) // count를 3으로 나머지 연산을 했을 때, 값이 0이 나오는 경우
             {
                 Debug.Log("박수 짝!");
-                count++;
                 continue;
             }
 
             Debug.Log(count);
         }
     }
-
 }
+
